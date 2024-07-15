4827d040306f2c39c550ca1677a92c7eaa9819d6
OchemetaSaint <137083302+OchemetaSaint@users.noreply.github.com>
2024-07-15 17:38:56 +0100

    fixe

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,56 +1,78 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { CgMenu } from "react-icons/cg";
-import { AiFillCloseCircle } from "react-icons/ai";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   const pathname = usePathname();
-
 
   const getLinkClasses = (path) => {
     return pathname === path
-      ? "text-red-500 lg:text-xl md:text-sm text-sm hover:text-green-500 border-b-2 border-red-500 px-2"
-      : "lg:text-[#081a1c] hover:text-green-500 px-2 lg:text-xl md:text-sm text-sm ";
+      ? "block py-2 px-3 md:p-0 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:dark:text-green-500 dark:bg-green-600 md:dark:bg-transparent"
+      : "block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
   };
 
-  const getButtonClasses = (path) => {
-    return pathname === path
-      ? "lg:px-10 md:px-4 px-2 bg-red-600 py-3 text-white rounded-md hover:bg-red-200 hover:text-red-900 text-lg md:text-sm text-xs  "
-      : "lg:px-10 md:px-4 px-2 bg-green-600 py-3 text-white rounded-md hover:bg-green-200 hover:text-green-900 text-lg md:text-sm text-xs";
-  };
-
-  return (
+   return (
     <>
-      <section className="shadow-lg lg:px-20  md:px-10 px-2 h-20 flex justify-between items-center fixed top-0 w-full bg-white z-50 ">
-        <article className="">
-          <Link href="/">
-            <h3 className="lg:text-2xl md:text-sm text-xs text-[#081a1c]">
+      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 sticky top-0 z-50">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <h3 className="lg:text-xl md:text-sm text-xs text-[#081a1c]">
               KROY<span className="text-[#04d15a]">Health</span>
             </h3>
           </Link>
-        </article>
-
-        <nav className="flex lg:gap-8 md:gap-4 gap-1">
-          <Link href="/" className={getLinkClasses("/")}>
-            Home
-          </Link>
-          <Link href="/about" className={getLinkClasses("/about")}>
-            About Us
-          </Link>
-          <Link href="/product" className={getLinkClasses("/product")}>
-            Products
-          </Link>
-        </nav>
-        <article>
-          <Link href="/contact">
-            <button className={getButtonClasses("/contact")}>
-              Contact
-            </button>
-          </Link>
-        </article>
-      </section>
-
+          <button
+            data-collapse-toggle="navbar-solid-bg"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-solid-bg"
+            aria-expanded={menuOpen}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className={`${menuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-solid-bg">
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+              <li>
+                <Link href="/" className={getLinkClasses("/")} aria-current="page">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/about" className={getLinkClasses("/about")}>
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link href="/product" className={getLinkClasses("/product")}>
+                  Product
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact" className={getLinkClasses("/contact")}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
